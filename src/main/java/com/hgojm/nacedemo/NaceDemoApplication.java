b6e5cf4310a3de193b20f90638e48174637f9300
Hugo Jimenez <hgojm.dev@gmail.com>
2022-08-20 13:58:40 -0500

    Initial commit

---
+++ src/main/java/com/hgojm/nacedemo/NaceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hgojm.nacedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgojm/nacedemo/NaceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgojm.nacedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
